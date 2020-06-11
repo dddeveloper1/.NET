f1d347a6669fa8f91b5c20afbeba7a124f1aae15
dddeveloper1 <tolikpopchuk@gmail.com>
2020-06-11 18:49:22 +0300

    added Exam

---
+++ WPF_Exam/WPF_13_Exam/Pages/VSMac.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_13_Exam;
+
+namespace WPF_13_Exam.Pages
+{
+    /// <summary>
+    /// Interaction logic for VSMac.xaml
+    /// </summary>
+    public partial class VSMac : Page
+    {
+        public VSMac()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start((sender as Hyperlink).Tag.ToString());
+
+
+
+        }
+
+        private void Hyperlink_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
